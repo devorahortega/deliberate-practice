86d13200b7c508e79fb9dbd96415a0671e35e9ab
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-13 19:07:22 -0500

    Adds january 13 practice

---
+++ jan-thirteen.rb
@@ -0,0 +1,9 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
