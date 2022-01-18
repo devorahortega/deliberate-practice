9c45b6f51a7c39bf15bcc34c09d23ee2fa67be1d
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-17 19:15:56 -0500

    Adds january seventeen deliberate practice

---
+++ jan-seventeen.rb
@@ -0,0 +1,73 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+# puts "Please enter a word:"
+# word = gets.chomp
+
+# puts word.upcase
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# puts "Please enter a number:"
+# num = gets.chomp
+
+# if num.to_i > 100
+#   p "Thats a big number"
+# end
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+# sum = 0
+
+# 2.times do
+#   puts "Please enter a number:"
+#   num = gets.chomp
+#   sum += num.to_i
+# end
+
+# p sum
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# puts "Please enter a word:"
+# word = gets.chomp
+
+# puts word.reverse
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# puts "Please enter a number:"
+# num = gets.chomp
+
+# 10.times do
+#   puts num
+# end
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+# phrase = ""
+
+# 2.times do
+#   puts "Please enter a word:"
+#   word = gets.chomp
+#   phrase += word.upcase + " "
+# end
+
+# puts phrase
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# puts "Please enter a word:"
+# word = gets.chomp
+
+# puts word.length
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# puts "Please enter a number:"
+# num = gets.chomp
+
+# if num.to_i < 0
+#   puts "That's a negative number"
+# end
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
