442e297436ca050106f18fd4c7f55b75205fcc2c
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-18 21:51:16 -0500

    Adds 10 minutes of deliberate practice

--- january-eighteen.rb
+++ january-eighteen.rb
@@ -1,12 +1,48 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
+# array = ["dog", "cat", "fish"]
+
+# array << "bird"
+
+# array << "horse"
+
+# puts array
+
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+# array = ["a", "b", "c", "d"]
+
+# array[1] = "f"
+
+# p array
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
+# array = [1, 2, 3, 4, 5]
+# index = 0
+
+# while array.length > index
+#   p array[index]
+#   index += 1
+# end
+
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
+# array = [1]
+
+# array << 2
+# array << 3
+# array << 4
+
+# p array
+
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+# array = ["apple", "banana", "orange"]
+
+# array[2] = array[2].upcase
+
+# p array
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
