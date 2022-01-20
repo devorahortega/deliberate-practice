5ce9c4c1fe4ab9ec144c8eb2a4dd8f606952b9d0
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-19 22:11:17 -0500

    Finishes problems

--- january-eighteen.rb
+++ january-eighteen.rb
@@ -46,10 +46,44 @@
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
+# array = ["Bob", "Emma", "Jane"]
+# index = 0
+
+# while array.length > index
+#   puts array[index]
+#   index += 1
+# end
+
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+# array = ["green", "blue"]
+
+# array << "orange"
+
+# p array
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
+# array = [1, 2, 3, 4, 5]
+
+# array[0] = array[0] * 10
+
+# p array
+
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
+# array = [1, 2]
+# index = 0
+
+# while array.length > index
+#   p array[index]
+#   index += 1
+# end
+
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+# array = ["Canada", "Mexico", "France"]
+
+# array << "England"
+
+# p array
