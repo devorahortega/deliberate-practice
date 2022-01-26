b7f374af7c60a35a17da15ff8b486b23b27a6c04
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-26 10:37:53 -0600

    Adds my confusion on the hard problems

--- hard-problems.rb
+++ hard-problems.rb
@@ -1,5 +1,15 @@
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
+while word == word
+  puts "Please enter a word"
+  word = gets.chomp
+  if word == "stop"
+    break
+  else
+    p word
+  end
+end
+
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
