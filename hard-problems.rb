7a1a4d72bd2797192bbe76d3079b29dc8bcc3d72
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-26 10:32:21 -0600

    Adds hard problems page for things i didnt learn yet

---
+++ hard-problems.rb
@@ -0,0 +1,5 @@
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
---
+++ january-twentysix.rb
@@ -0,0 +1,55 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+# index = 1
+
+# while index < 11
+#   p index
+#   index += 1
+# end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+# index = 0
+
+# while index < 6
+#   p "hello"
+#   index += 1
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+# index = 0
+
+# while index < 101
+#   p index
+#   index += 5
+# end
+
+# # 5. Write a while loop that prints the number 9000 ten times.
+# index = 0
+
+# while index < 11
+#   p 9000
+#   index += 1
+# end
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+# index = 50
+
+# while index < 71
+#   p index
+#   index += 1
+# end
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+# index = 0
+
+# while index < 145
+#   p "Around the world"
+#   index += 1
+# end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+# index = 2
+
+# while index < 41
+#   p index
+#   index += 1
+# end
