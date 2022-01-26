b58fff0c028611ecc8e57b296399207b42536c30
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-26 10:54:57 -0600

    Solves binary problems

--- binary.rb
+++ binary.rb
@@ -1,1 +1,14 @@
 p "%b" % 245
+
+def convert_to_binary(integer)
+  binary = []
+  while integer > 0
+    binary << integer % 2
+    integer /= 2
+  end
+  binary.reverse.join
+end
+
+p convert_to_binary(1356)
+p convert_to_binary(2289)
+p convert_to_binary(16563)
