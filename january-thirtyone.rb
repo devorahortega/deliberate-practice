07b44b8c8a6d8c82c5c6ccdbf05868cf5dfaaf99
Devorah Ortega <devorahb.ortega@gmail.com>
2022-02-01 21:25:52 -0600

    Adds work for february 1

--- january-thirtyone.rb
+++ january-thirtyone.rb
@@ -45,12 +45,27 @@
 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
-array = [2, 4, 5, 1, 8, 9, 7]
+# array = [2, 4, 5, 1, 8, 9, 7]
+# array2 = []
+# index = 1
+
+# while index < array.length
+#   if array[index] % 2
+#     array2 << array[index]
+#   end
+#   index += 1
+# end
+
+# p array2
+
+#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+#     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+index = 0
+array = ["a", "man", "a", "plan", "a", "canal", "panama"]
 array2 = []
-index = 1
 
 while index < array.length
-  if array[index] % 2
+  if array[index].length < 4
     array2 << array[index]
   end
   index += 1
@@ -58,11 +73,14 @@ end
 
 p array2
 
-#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
-#     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
-
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+index = 0
+array = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+array2 = []
 
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+index = 0
+array = [8, 23, 0, 44, 1980, 3]
+array2 = []
