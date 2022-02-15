ae6581ffee302a8037334f2f833cf9ed6e353516
Devorah Ortega <devorahb.ortega@gmail.com>
2022-02-14 21:59:02 -0600

    Adds today's practice

---
+++ february-fourteen.rb
@@ -0,0 +1,49 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+index = 1
+
+while index < 11
+  p index
+  index += 1
+end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+index = 0
+
+while index < 5
+  p "hello"
+  index += 1
+end
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+index = 0
+
+puts "Enter a word:"
+word = gets.chomp
+
+while word != "stop"
+  word = word
+  if word == "exit"
+    break
+  end
+  p word
+end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+index = 0
+
+while index < 101
+  p index
+  index += 5
+end
+
+# 5. Write a while loop that prints the number 9000 ten times.
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
