d803973f993ba739af76834730dc1361021f0a65
Devorah Ortega <devorahb.ortega@gmail.com>
2022-02-16 22:53:37 -0600

    Adds today's practice

--- february-sixteen.rb
+++ february-sixteen.rb
@@ -1,17 +1,63 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
+def times_two(num)
+  sum = num * 2
+  return sum
+end
+
+p times_two(2)
+
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+def capitalize(word)
+  return word.upcase
+end
+
+p capitalize("unicorn")
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
+def subtraction(num1, num2)
+  sum = num1 - num2
+  return sum
+end
+
+p subtraction(6, 4)
+
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+def multiple(num)
+  sum = num * num
+  p sum
+end
+
+p multiple(2)
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
+def first_letter(word)
+  return word[0]
+end
+
+p first_letter("apple")
+
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+def combine(string1, string2, string3)
+  word = string1 + string2 + string3
+  return word
+end
+
+p combine("this", "is", "word")
 
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
+def stringefy(num)
+  string = num.to_s
+  return string
+end
+
+p stringefy (5)
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
