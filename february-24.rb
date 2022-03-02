6300afff9224cf36cb065613eb9dfe045996f3a8
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-01 23:22:10 -0600

    Adds last problems in this page

--- february-24.rb
+++ february-24.rb
@@ -92,6 +92,14 @@ end
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+array = [1, 2, 3]
+array2 = []
+
+array.each do |num|
+  array2 << num.to_s
+end
+
+p array2
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/b9ac4390aad2301a2238efc95c904f3d
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/66598fd7cdbc67fe663624e217cebbaf
