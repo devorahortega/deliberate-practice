e27cd7a67cb279c0f1c92957ac3d8732cf5b67ad
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-03 21:49:29 -0600

    Adds today's pracice problems

--- march-02.rb
+++ march-02.rb
@@ -1,14 +1,51 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+# puts "enter a word"
+# word = gets.chomp
+
+# puts word.upcase
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+# puts "enter number"
+# num = gets.chomp
+
+# if num.to_i > 100
+#   p "That's a bit number"
+# end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+# sum = 0
 
+# 2.times do
+#   puts "enter a number"
+#   num = gets.chomp
+#   sum += num.to_i
+# end
+
+# p sum
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+# puts "enter word"
+# word = gets.chomp
+
+# puts word.reverse
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
+# puts "enter a number"
+# num = gets.chomp
+
+# 10.times do
+#   p num
+# end
 
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+# sentence = ""
+
+# 2.times do
+#   puts "enter word"
+#   word = gets.chomp
+#   sentence += word.upcase + " "
+# end
+
+# p sentence
 
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
 
