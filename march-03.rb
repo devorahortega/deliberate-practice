679d957ce634182bb385db90d85b0acc4668e89d
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-09 10:39:25 -0600

    adds today's practice

--- march-03.rb
+++ march-03.rb
@@ -27,13 +27,38 @@
 # end
 
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+# array = [{ title: "book", author: "writer", page_numer: 123, language: "english" }, { title: "book2", author: "writer2", page_numer: 1232, language: "englishe" }, { title: "book3", author: "writer3", page_numer: 1233, language: "englishee" }]
+
+# p array[2][:author]
 
 # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+# hash = { idk: "captial", canadian: "dunnostates", noamericano: "notsure" }
+
+# hash[:ummm] = "ahh"
+
+# p hash
 
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+# hash = { brand: "Asus", model: "thisone", year: 2030 }
+
+# hash.each do |thing|
+#   p thing
+# end
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+# array = [{ brand: "Asus", model: "thisone", year: 2030 }, { brand: "Asuss", model: "thisonetwo", year: 2032 }, { brand: "Asusss", model: "thisonethree", year: 2033 }]
+
+# p array[1][:model]
 
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+hash = { apple: "fruit", bagel: "bread" }
 
+hash[:cucumber] = "vegetable"
+
+p hash
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+hash = { brand: "nike", color: "red", size: "XXL" }
+
+p hash[:brand]
+p hash[:color]
+p hash[:size]
