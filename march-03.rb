63a45bd3da614fe8f908b34c46b359110760a1ad
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-08 22:51:34 -0600

    Adds todays problems

--- march-03.rb
+++ march-03.rb
@@ -1,15 +1,30 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
-hash = { first_name: "jon", last_name: "green", email: "jongren@example.com" }
+# hash = { first_name: "jon", last_name: "green", email: "jongren@example.com" }
 
-hash.each do |thing|
-  p thing
-end
+# hash.each do |thing|
+#   p thing
+# end
 
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+# array = [{ first_name: "jon", last_name: "green", email: "jongren@example.com" }, { first_name: "bob", last_name: "green", email: "bobgren@example.com" }, { first_name: "jane", last_name: "green", email: "janegren@example.com" }]
+
+# array.each do |hash|
+#   p hash
+# end
 
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+# hash = { croc: 12, tree: 123, apple: 5 }
+
+# hash.each do |thing|
+#   p thing
+# end
 
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+# hash = { title: "book", autor: "writer", page_numer: 123, language: "english" }
+
+# hash.each do |thing|
+#   p thing
+# end
 
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
