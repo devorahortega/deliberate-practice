77ddafcb849860c3a6dd44ff82288369f183d2bd
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-10 22:29:10 -0600

    Adds today's practice

--- march-10.rb
+++ march-10.rb
@@ -1,8 +1,32 @@
 #  1. Convert an array of arrays into a hash.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
+# array = [[1, 3], [8, 9], [2, 16]]
+# hash = {}
+# index = 0
+
+# while array.length > index
+#   key = array[index][0]
+#   value = array[index][1]
+#   hash[key] = value
+#   index += 1
+# end
+
+# p hash
+
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+
+# array = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
+# hash = {}
+# index = 0
+
+# while index < array.length
+#   hash[array[index][:id]] = array[index]
+#   index += 1
+# end
+
+# p hash
 
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
