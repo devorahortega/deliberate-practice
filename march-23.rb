e7086f299341d9e22431d6632d3449083bc35da1
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-23 11:02:45 -0500

    Adds today's problems

--- march-23.rb
+++ march-23.rb
@@ -1,12 +1,54 @@
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+num = 4
 
-# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+if num == 10
+  p 0
+else
+  p -1
+end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to
+num = 2
+
+if num < 10
+  p -1
+elsif num > 10
+  p 1
+elsif num == 10
+  p 0
+end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+num = 2
+num2 = 4
+
+if num < 10 && num2 < 10
+  p 1
+else
+  p 0
+end
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+num = 1234
+
+if num > 9000
+  p 1
+else
+  p -1
+end
 
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+num = 6
+
+if num < 10
+  p 9
+elsif num < 20
+  p 19
+elsif num < 30
+  p 29
+else
+  p -1
+end
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
