becb7a94fddb86aa572bdcb3284704fa76c32c7c
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-24 19:45:31 -0500

    Adds today's practice

--- march-23.rb
+++ march-23.rb
@@ -51,11 +51,56 @@ else
 end
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+num1 = 5
+num2 = 6
+
+if num1 > 10 || num2 > 10
+  p 100
+else
+  p -100
+end
 
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+num = 1003
+
+if num < 0
+  p 1776
+else
+  p 1779
+end
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+num = 899
+
+if num == 100
+  p 100
+elsif num == 99
+  p 99
+else
+  p 0
+end
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+num1 = 9
+num2 = 0
+
+if num1 < 0 && num2 > 0
+  p 1
+else
+  p 0
+end
 
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+num = 8
+
+if num > 80
+  p 5
+elsif num > 60
+  p 4
+elsif num > 40
+  p 3
+elsif num > 20
+  p 2
+else
+  p 1
+end
